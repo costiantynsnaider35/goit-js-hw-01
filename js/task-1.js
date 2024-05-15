174393608cd63dd530faf1cf292d7d19ba7d6e0b
Kostiantyn Shnaider <constantin161089@gmail.com>
2024-05-15 18:07:39 +0300

    дз1

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+function makeTransaction(quantity, pricePerDroid) {
+  return `You ordered ${quantity} droids worth ${pricePerDroid} credits!`;
+
+  makeTransaction(5, 3000);
+  makeTransaction(3, 1000);
+  makeTransaction(10, 500);
+}
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
